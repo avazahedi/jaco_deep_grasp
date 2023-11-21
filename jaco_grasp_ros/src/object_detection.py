913d0f38dbdfc495a72c70a409e3fecc6ffe0db7
Ava Zahedi <AvaZahedi2023@u.northwestern.edu>
2023-11-21 11:22:49 -0600

    created custom message type for bounding box info. modified object_detection and traj_plot nodes

--- jaco_grasp_ros/src/object_detection.py
+++ jaco_grasp_ros/src/object_detection.py
@@ -7,16 +7,22 @@ import numpy as np
 import rospy
 from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge
+from std_msgs.msg import Float32MultiArray
+from jaco_grasp_ros_interfaces.msg import BboxCoords
 # import torch
 # import rospkg
 
 class ObjectDetection:
     def __init__(self):
-        # realsense subscriber
+        # realsense subscribers
         self.rgb_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.rgb_callback)
-        self.depth_sub = rospy.Subscriber("/camera/depth/image_rect_raw", Image, self.depth_callback)
+        # self.depth_sub = rospy.Subscriber("/camera/depth/image_rect_raw", Image, self.depth_callback)
+        self.depth_sub = rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.depth_callback)
         self.intr_sub = rospy.Subscriber("/camera/color/camera_info", CameraInfo, self.intr_callback)
-        self.timer = rospy.Timer(rospy.Duration(1), self.timer_callback)
+
+        # passthrough filter publisher
+        self.passthrough_pub = rospy.Publisher("/passthrough_filter_vals", BboxCoords, queue_size=10)
+        self.passthrough_vals = BboxCoords()
 
         self.intr = None
         self.color_image = None
@@ -33,25 +39,24 @@ class ObjectDetection:
         # print(model_path)
         # self.results = self.model.train(data=model_path, epochs=100, imgsz=640)
 
+        # bottle, cup, laptop
         self.desired_object = "bottle"  # this will be able to be set by user
 
-    def timer_callback(self):
-        pass
+        # print(self.model.names)
 
-    def rgb_callback(self, msg):
-        self.color_image = self.bridge.imgmsg_to_cv2(msg)
-        self.color_image = cv2.cvtColor(self.color_image, cv2.COLOR_BGR2RGB) # convert BGR to RGB
-        cv2.imshow("raw image", self.color_image)
+        self.frequency = 25.0
+        self.timer = rospy.Timer(rospy.Duration(1.0/self.frequency), self.timer_callback)
 
-        # self.results = self.model(image)   # same as self.model.predict(image)
+    def timer_callback(self, event=None):
+        if self.intr is None or self.color_image is None or self.depth_image is None:
+            return
+
         self.results = self.model.predict(self.color_image, conf=0.7, verbose=False, save_txt=False)
         annotated_img = self.results[0].plot()
         cv2.imshow("YOLOv8 Inference", annotated_img)   # same as show=True in self.model.predict
 
         # print(self.results[0].boxes.xywhn) # x_center, y_center, width, height (normalized by original image size)
         # print(self.results[0].boxes.xywh) # x_center, y_center, width, height
-
-        # print(self.model.names)
 
         # for r in self.results:
         #     for c in r.boxes.cls:
@@ -63,33 +68,68 @@ class ObjectDetection:
             for c in r.boxes.cls:
                 if (self.model.names[int(c)]) == self.desired_object:
                     xywh = r.boxes.xywh.tolist()[0]     # unpacking PyTorch Tensor
-                    xcenter, ycenter, width, height = xywh
+                    # xcenter, ycenter, width, height = xywh
+                    xcenter, ycenter, width, height = [int(i) for i in xywh]
 
-                    print(f"xcenter: {xcenter}")
-                    print(f"ycenter: {ycenter}")
+                    # print(f"xcenter: {xcenter}")
+                    # print(f"ycenter: {ycenter}")
+                    # print(f"width: {width}")
+                    # print(f"height: {height}")
+
+                    # print(f"self.intr.width: {self.intr.width}")
+                    # print(f"self.intr.height: {self.intr.height}")
 
                     # either send xywh values to grasp_detection.cpp for processing,
                     # or process values before sending
-                    xright = xcenter + width/2.0
-                    xleft = xcenter - width/2.0
-                    ytop = ycenter + height/2.0
-                    ybot = ycenter - height/2.0
 
-                    rs.rs2_deproject_pixel_to_point()
+                    # pixel values to be deprojected [xleft, xright, ytop, ybottom]
+                    xleft = int(xcenter - width/2)
+                    xright = int(xcenter + width/2)
+                    ytop = int(ycenter + height/2)
+                    ybottom = int(ycenter - height/2)
 
-                    pen_coords_wrt_camera = rs.rs2_deproject_pixel_to_point(intr, [centroid_x, centroid_y], pen_depth)
+                    # print(f"xright: {xright}")
+                    # print(f"xleft: {xleft}")
+                    # print(f"ytop: {ytop}")
+                    # print(f"ybottom: {ybottom}")
+                    # print(f"right depth value: {self.depth_image[int(ycenter/1.5)][int(xright/1.509)]}")
 
+                    # use depth of center of bounding box
+                    depth = self.depth_image[int(ycenter)][int(xcenter)]
 
-        # press ESC or 'q' to close
+                    if depth == 0:
+                        return
+
+                    # convert pixels to points
+                    # each point is a list [x, y, z] coordinates
+                    left = rs.rs2_deproject_pixel_to_point(self.intr, [xleft, ycenter], depth)
+                    right = rs.rs2_deproject_pixel_to_point(self.intr, [xright, ycenter], depth)
+                    top = rs.rs2_deproject_pixel_to_point(self.intr, [xcenter, ytop], depth)
+                    bottom = rs.rs2_deproject_pixel_to_point(self.intr, [xcenter, ybottom], depth)
+
+                    # convert to m
+                    self.passthrough_vals.left = [i/1000. for i in left]
+                    self.passthrough_vals.right = [i/1000. for i in right]
+                    self.passthrough_vals.top = [i/1000. for i in top]
+                    self.passthrough_vals.bottom = [i/1000. for i in bottom]
+                    self.passthrough_pub.publish(self.passthrough_vals)
+
+        # press ESC or 'q' to closearray
         key = cv2.waitKey(1)
         if key & 0xFF == ord('q') or key == 27:
             cv2.destroyAllWindows()
 
 
+    def rgb_callback(self, msg):
+        self.color_image = self.bridge.imgmsg_to_cv2(msg)
+        self.color_image = cv2.cvtColor(self.color_image, cv2.COLOR_BGR2RGB) # convert BGR to RGB
+        # cv2.imshow("raw image", self.color_image)
+
+
     def depth_callback(self, msg):
         # Get depth image
         self.depth_image = self.bridge.imgmsg_to_cv2(msg)
-
+        # self.depth_image is of size (480, 848)
 
     def intr_callback(self, msg):
         # Set camera intrinsics
@@ -111,10 +151,11 @@ class ObjectDetection:
 
 
 def main():
-    obj_det = ObjectDetection()
-
     # initialize the object detection node
     rospy.init_node("object_detection", anonymous=True)
+
+    obj_det = ObjectDetection()
+
     rospy.spin()
 
 if __name__ == '__main__': 
--- jaco_grasp_ros/src/traj_plot.py
+++ jaco_grasp_ros/src/traj_plot.py
@@ -4,13 +4,13 @@ import rospy
 from control_msgs.msg import FollowJointTrajectoryFeedback
 import matplotlib.pyplot as plt
 from std_msgs.msg import Int8
+from datetime import datetime
 # from IPython import embed
 
 
 class TrajSubscriber: 
 
     def __init__(self): 
-        print("Initializing the instance!") 
         self.traj_sub = rospy.Subscriber("/j2s7s300_driver/trajectory_controller/state", 
                                         FollowJointTrajectoryFeedback, self.traj_callback)
         
@@ -37,7 +37,7 @@ class TrajSubscriber:
         #             FollowJointTrajectoryFeedback) 
 
         if self.executing.data == 1:
-            rospy.loginfo("Saving trajectory data...")
+            # rospy.loginfo("Saving trajectory data...")
             self.seq.append(msg.header.seq)
             for i in range(7):
                 self.desired[i].append(msg.desired.positions[i])
@@ -56,26 +56,28 @@ class TrajSubscriber:
             rospy.loginfo("Plotting trajectory")
             # print(f"self.seq: ", self.seq)
             # print(f"self.desired: ", self.desired)
+            fig = plt.figure("Joint Trajectories")
             for i in range(7):
-                fig0 = plt.figure(f"Joint {i+1}")
+                # fig0 = plt.figure(f"Joint {i+1}")
                 plt.subplot(7, 2, 2*i+1)
-                plt.plot(self.seq, self.desired[i], 'g', label="desired")
-                plt.plot(self.seq, self.actual[i], 'b', label="actual")
+                plt.plot(self.seq, self.desired[i], 'g')
+                plt.plot(self.seq, self.actual[i], 'b')
+                # plt.plot(self.seq, self.desired[i], 'g', label=f"desired j{i}")
+                # plt.plot(self.seq, self.actual[i], 'b', label=f"actual j{i}")
                 plt.legend()
 
                 # fig1 = plt.figure(f"Error for joint {i+1}")
                 plt.subplot(7, 2, 2*i+2)
                 plt.plot(self.seq, self.error[i], 'r', label="error")
             plt.legend()
-            plt.savefig("joint_trajectories.png")
+
+            file_name = "joint_trajectories " + str(datetime.now()) + ".png"
+            plt.savefig(file_name)
             plt.show()
 
 def main(): 
     # create a subscriber instance 
     sub = TrajSubscriber() 
-    
-    # follow it up with a no-brainer sequence check 
-    print('Currently in the main function...') 
     
     # initializing the subscriber node 
     rospy.init_node('listener', anonymous=True) 
